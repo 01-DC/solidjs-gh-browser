ef76eeadf49ff38f69b1e3406323d96b284eff98
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-12-19 13:00:00 +0530

    setup data flow into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { repos, setUsername, username } from "../App"
+
 function Home() {
+	const refetchWithUsername = (event) => {
+		event.preventDefault()
+		setUsername(event.target.username.value)
+	}
 	return (
 		<div>
-			<h2>Home</h2>
+			<form class="mb-3" onSubmit={(event) => refetchWithUsername(event)}>
+				<input
+					type="text"
+					class="p-1 align-middle"
+					name="username"
+					required
+				/>
+				<button class="btn btn-dark ms-3 w-auto">Fetch</button>
+			</form>
+			<h2>Github repos for {username()}</h2>
 		</div>
 	)
 }
